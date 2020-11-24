0397748aa7fabb84e0b903ea207b8e74b1893b83
Stas(Lexa) <44728228+igedudilu@users.noreply.github.com>
2020-11-24 21:08:40 +0500

    вроде норм

--- laba oop 2.1.cpp
+++ laba oop 2.1.cpp
@@ -1,36 +1,8 @@
 ﻿#include <iostream>
 #include <conio.h>
+#include "Point.h"
+
 using namespace std;
-
-class Point {
-public:
-	int x, y;
-	Point() {
-		printf("Point()\n");
-		x = 0;
-		y = 0;
-	}
-	Point(int x, int y) {
-		printf("Point(int x, int y)\n");
-		this->x = x;
-		this->y = y;
-	}
-	Point(const Point& p) {
-		printf("Point(const Point&p)\n");
-		x = p.x;
-		y = p.y;
-	}
-	~Point() {
-		printf("%d, %d\n", x, y);
-		printf("~Point()\n");
-	}
-	void move(int x, int y) {
-		this->x += x;
-		this->y += y;
-	}
-	void randomc();
-
-};
 
 void Point::randomc() {
 	y = rand() % 20;
